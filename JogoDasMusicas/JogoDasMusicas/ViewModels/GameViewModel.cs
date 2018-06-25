75954a921b7817889294e0e0c75cc9f39e6ad02d
Mauricio <mauricio.fjo@hotmail.com>
2018-06-25 20:00:01 -0300

    Music and lyric sincronized

--- JogoDasMusicas/JogoDasMusicas/ViewModels/GameViewModel.cs
+++ JogoDasMusicas/JogoDasMusicas/ViewModels/GameViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using JogoDasMusicas.Models;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Timers;
 
 namespace JogoDasMusicas.ViewModels
 {
@@ -12,6 +14,8 @@ namespace JogoDasMusicas.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public List<Lyric> Lyrics;
+        private Timer LyricTimer;
+        private int CurrentVerseIndex;
 
         public GameViewModel ()
         {
@@ -28,6 +32,31 @@ namespace JogoDasMusicas.ViewModels
             var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
             player.Load(audioStream);
             player.Play();
+
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            LyricTimer = new Timer(10000);
+            LyricTimer.Elapsed += OnFinishTime;
+            LyricTimer.Enabled = true;
+        }
+
+        private void OnFinishTime(object sender, ElapsedEventArgs e)
+        {
+            if (CurrentVerseIndex == Lyrics.Count)
+            {
+                CurrentVerse = string.Empty;
+                LyricTimer.Enabled = false;
+            }
+            else
+            {
+                int lastTime = CurrentVerseIndex == 0 ? 10 : Lyrics[CurrentVerseIndex - 1].EndsIn;
+                int currentTime = Lyrics[CurrentVerseIndex].EndsIn;
+                CurrentVerse = Lyrics[CurrentVerseIndex++].Verse;
+                LyricTimer.Interval = (currentTime - lastTime) * 1000;
+            }
         }
 
         void OnPropertyChanged([CallerMemberName] string name = "")
