de7406c06c7b35b980dae6d72b884354f4a1b3c6
Maurício Feijó <mauricio@flipstudio.net>
2018-06-30 11:52:02 -0300

    Promob saturday

--- JogoDasMusicas/JogoDasMusicas/Views/GamePage.xaml.cs
+++ JogoDasMusicas/JogoDasMusicas/Views/GamePage.xaml.cs
@@ -9,11 +9,22 @@ namespace JogoDasMusicas.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class GamePage : ContentPage
 	{
+        private Entry AnswerEntry2;
 		public GamePage ()
 		{
 			InitializeComponent ();
 
-            BindingContext = new GameViewModel(); 
+            BindingContext = new GameViewModel();
+            AnswerEntry2 = this.FindByName<Entry>("AnswerEntry");
+            this.FindByName<Button>("myButton").IsEnabled = false;
+
+            Appearing += GetEntryFocus;
+            AnswerEntry2.Unfocused += GetEntryFocus;
+        }
+
+        private void GetEntryFocus(object sender, EventArgs e)
+        {
+            if (AnswerEntry2.IsVisible) AnswerEntry2.Focus();
         }
     }
 }
